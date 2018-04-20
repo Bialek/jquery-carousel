49a6924dece057a67f0d09d33afd3e8df4e93137
Rafał Białek <bialekrafal94@gmail.com>
2018-04-20 11:46:23 +0200

    add control arrows

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,54 @@
 $(function(){
+
 	var list = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+	var interval = setInterval(changeSlide, 3000);
+
 	function changeSlide(){
-		list.animate({"marginLeft":-400}, 500, moveFirstSlide);
+		clearTimeout(interval);
+		list.animate({"marginLeft":-2400}, 500, moveFirstSlide);
+
 		function moveFirstSlide(){
 			var firstItem = list.find("li:first");
 			var lastItem = list.find("li:last");
 			lastItem.after(firstItem);
-			list.css({marginLeft:0});
+			list.css({marginLeft:-1200});
 		}
 
+		interval = setInterval(changeSlide, 3000);
+	}
+
+	$("#carousel").mouseout(function(){
+		$("#carousel i").css("opacity","1");
+	});
+
+	$("#carousel").mouseover(function(){
+		$("#carousel i").css("opacity","1"); 
+	});
+
+	$("#right").click(changeSlide);
+
+	$("#left").click(function(){
+		list.animate({"marginLeft":0}, 500, moveLastSlide);
+		clearTimeout(interval);
+
+		function moveLastSlide(){
+
+			for(var i = 0; i <= 3;i++){
+				var firstItem = list.find("li:first");
+				var lastItem = list.find("li:last");
+				lastItem.after(firstItem);
+
+			}
+			
+			list.css({marginLeft:-1200});
+		}
+
+		interval = setInterval(changeSlide, 3000);
+	});
+	var circle = $('[class*="circle"]');
+	console.log(circle);
+	for(var i = 0;i <= circle.length; i++){
+		circle.css("left", 40 + (i*2));
 	}
 });
+
