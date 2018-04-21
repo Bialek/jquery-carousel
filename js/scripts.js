ff85f0bee3286d56444cc82f5d32ee725ab5c169
Rafał Białek <bialekrafal94@gmail.com>
2018-04-21 13:13:25 +0200

    fix dots

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,31 @@ $(function(){
 	var list = $("#carousel ul");
 	var interval = setInterval(changeSlide, 3000);
 
-	function changeSlide(){
+	var slidesCount = list.find('li').toArray().length;
+	var $dots = $('<div class="dots"></div>');
+
+	var $dotsArray = [], counter = 10000 * slidesCount;
+
+	for (var i = 1; i <= slidesCount; i++) {
+		var $button = $('<button>'+i+'</button>');
+
+		$dotsArray.push($button);
+
+		$dots.append($button);
+	}
+
+	function activeDot() {
+		$('button.active').removeClass('active');
+		$dotsArray[counter % slidesCount].addClass('active');
+	}
+
+	activeDot();
+
+	$("#carousel").append($dots);
+
+
+	function changeSlide(msTime){
+		msTime = msTime || 500;
 		clearTimeout(interval);
 		list.animate({"marginLeft":-2400}, 500, moveFirstSlide);
 
@@ -12,10 +36,21 @@ $(function(){
 			var lastItem = list.find("li:last");
 			lastItem.after(firstItem);
 			list.css({marginLeft:-1200});
+
+			counter++;
+			activeDot();
 		}
 
 		interval = setInterval(changeSlide, 3000);
 	}
+
+	// function moveXTimes(moves) {
+	// 	for (var i = 0; i < moves; i++) {
+	// 		setTimeout(function () {
+	// 			changeSlide(500 / moves);
+	// 		}, i * 500 / moves);
+	// 	}
+	// }
 
 	$("#carousel").mouseout(function(){
 		$("#carousel i").css("opacity","1");
